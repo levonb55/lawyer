7caf7e360f32eef68ac5bbabe44fed7e3cc503d8
Levon Baghdasaryan <levonbaghdasarian@gmail.com>
2019-06-18 18:59:34 +0400

    PublicationController created

--- app/Http/Controllers/LawyerController.php
+++ app/Http/Controllers/LawyerController.php
@@ -83,31 +83,4 @@ class LawyerController extends Controller
         return response()->json($reviews);
     }
 
-    public function storePublications(StorePublication $request,User $user) {
-
-        $quant = count($request->title);
-
-        for ($i = 0; $i < $quant; $i++) {
-            if($request->publication[$i]) {
-                $fileName = time(). $i . '.' . $request->publication[$i]->extension();
-                $location = $request->publication[$i]->move(public_path('assets/publications/'), $fileName);
-                chmod($location,0777);
-            }
-
-            Publication::create([
-                'user_id' => \Auth::id(),
-                'title' => $request->input('title')[$i],
-                'publication' => $fileName
-            ]);
-        }
-
-        Session::flash('success', 'You successfully edited your publications!');
-
-        return back();
-    }
-
-    public function getPublication(Publication $publication){
-
-        return view('lawyers.publication', compact('publication'));
-    }
 }
