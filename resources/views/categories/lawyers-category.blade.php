e53a647f1b0e31891cfc6c86851ede326822858a
James Doe <james@gmail.com>
2019-06-27 11:07:01 +0400

    Lawyers rating search commented

--- resources/views/categories/lawyers-category.blade.php
+++ resources/views/categories/lawyers-category.blade.php
@@ -24,15 +24,15 @@
                 </div>
                 <div class="find_2_left_select">
                     <p>We found {{$lawyers->count()}} professional lawyers</p>
-                    <form action="{{route('lawyers.rating')}}" method="POST" id="rating-form">
-                        @csrf
-                        <select name="rating" id="rating">
-                            <option>Rating</option>
-                            <option value="5" {{ old('rating') == 5 ? 'selected' : ''}}>5 star</option>
-                            <option value="4" {{ old('rating') == 4 ? 'selected' : ''}}>4 star</option>
-                            <option value="3" {{ old('rating') == 3 ? 'selected' : ''}}>3 star</option>
-                        </select>
-                    </form>
+{{--                    <form action="{{route('lawyers.rating')}}" method="POST" id="rating-form">--}}
+{{--                        @csrf--}}
+{{--                        <select name="rating" id="rating">--}}
+{{--                            <option>Rating</option>--}}
+{{--                            <option value="5" {{ old('rating') == 5 ? 'selected' : ''}}>5 star</option>--}}
+{{--                            <option value="4" {{ old('rating') == 4 ? 'selected' : ''}}>4 star</option>--}}
+{{--                            <option value="3" {{ old('rating') == 3 ? 'selected' : ''}}>3 star</option>--}}
+{{--                        </select>--}}
+{{--                    </form>--}}
                 </div>
                 <div class="find_2_left_block" id="style-2">
 
