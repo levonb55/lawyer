a46caa1d49818d5da4239a557720e746ab636f4d
Levon Baghdasaryan <levonbaghdasarian@gmail.com>
2019-08-11 16:33:52 +0400

    node_modules removed from gitignore

--- app/Events/NewMessage.php
+++ app/Events/NewMessage.php
@@ -34,7 +34,7 @@ class NewMessage implements ShouldBroadcast
      */
     public function broadcastOn()
     {
-        return new Channel('messages.' . $this->message->receiver_id);
+        return new Channel('messages.' . auth()->id());
     }
 
     public function broadcastWith() {
